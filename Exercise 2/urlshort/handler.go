c9860726a1d035efd955aa3c29f066d185794a3f
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-07-02 11:35:25 -0500

    minor change on if statement

--- Exercise 2/urlshort/handler.go
+++ Exercise 2/urlshort/handler.go
@@ -14,8 +14,7 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		path, ok := pathsToUrls[r.URL.Path]
-		if ok {
+		if path, ok := pathsToUrls[r.URL.Path]; ok {
 			http.Redirect(w, r, path, http.StatusFound)
 		} else {
 			fallback.ServeHTTP(w, r)
